fc6846269ed3bd57d1c8da183f618548a58fc958
Ciaran Darcy <cdarcy@ciarans-mbp.mul.ie.ibm.com>
2017-01-04 13:21:40 +0000

    graphing corrected can do multiple graphs now

--- cloudantAdd.js
+++ cloudantAdd.js
@@ -10,40 +10,44 @@ var api=process.env.api_key;
 var cloudant = Cloudant({account:username, password:password});
 var ciaran = cloudant.db.use('ciaran')
 
-var Indexes={name:"Beer Names", type:"json", index:{fields:["name","_id"]} }
 
 
 
-var beerName="Budwesier";
 
-var searchUrl="https://api.brewerydb.com/v2/search?q="+beerName+"&type=beer&key="+api+"&format=json"; 
-//console.log(searchUrl)
+var Currency="USD";
+var startDate=new Date(2000,02,01);
+
+var endDate=new Date(2016,12,31);
+var nextDate= startDate;
 
 
-request(searchUrl, function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-  //console.log(body)
-  var obj = JSON.parse(body)
-  var data=obj.data;
+
+while (nextDate < endDate){
   
+    nextDate.setDate(nextDate.getDate()+1);
+    dateISO=nextDate.toISOString().split("T")[0];
+    var searchUrl="http://api.fixer.io/"+dateISO; 
+    //console.log(searchUrl)
 
-  ciaran.insert(obj, function(err, body, header, cb ){
-      if (err) {
-        return console.log('[ciaran.insert] ', err.message);
-      }
 
-      console.log('You have inserted the document.');
-       })
-  };
+    request(searchUrl, function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        //console.log(body)
+        var obj = JSON.parse(body)
+        var id=obj.date
 
-  ciaran.index(Indexes, function(err, result){
-  if (err){
-    console.log (err);
-  }
-    
-})
+        ciaran.insert({ _id: id, obj}, function(err, body, header ){
+          if (err) {
+           return console.log('[ciaran.insert] ', err.message);
+          }
 
+          console.log('You have inserted the record for '+ id);
+          
+        })
   
-})
+      }; 
+    });
 
+}//close while
 
+/**/
--- cloudantSearch.js
+++ cloudantSearch.js
@@ -1,20 +1,69 @@
 require('dotenv').load();
 
 var Cloudant = require('cloudant');
-var request = require('request')
+var request = require('request');
+
 
 var username = process.env.cloudant_username;
 var password = process.env.cloudant_password;
 
+var plotly_username = process.env.plotly_username;
+var plotly_api_key = process.env.plotly_api_key;
 
 var cloudant = Cloudant({account:username, password:password});
 var ciaran = cloudant.db.use('ciaran');
+var currency ="USD";
+var currency_search="rows[row].doc.obj.rates."+currency
+//console.log(currency_search);
+  
+var plotly =require('plotly')(plotly_username, plotly_api_key);
 
+ciaran.list({include_docs:true}, function(err, data){
+	var rows = data.rows;
+	var count = 1;
+	var dates=[];
+	var usrates=[];
+	var gbprates=[];
 
-ciaran.get('Guinness', function(err,body){
-	if(!err){
-		console.log(body);
-	}else{
-		console.log(err);
+	for (row in rows){
+		
+		var date = rows[row].doc.obj.date;
+		var usrate= rows[row].doc.obj.rates.USD;
+		var gbprate= rows[row].doc.obj.rates.GBP;
+		//console.log (currency);
+		//console.log(dates+ " : "+currency_rates);
+		dates[row]=date;
+		usrates[row]=usrate;
+		gbprates[row]=gbprate;
+		//console.log(rates)
+
+		count ++; 
 	}
+	
+	var dataUS={
+		x:dates,
+		y:usrates,
+		type:"scatter",
+		name:"US Rates"
+	};
+	 var dataGBP={
+	 	x:dates,
+	 	y:gbprates,
+	 	type:"scatter",
+	 	name:"GBP Rates"
+	 };
+
+	 var data=[dataGBP,dataUS];
+
+	 
+
+	var graphOptions={
+		filename:"Euro Rate Performance 2000 - 2016",
+		fileopt:"overwrite"
+	}
+	plotly.plot(data, graphOptions, function(err, msg){
+		console.log(msg);
+	})
 })
+
+
